cb157596f70293d5f70b54561d8b0481faa3acb3
Enn18 <ennennuste3@gmail.com>
2022-09-14 20:05:23 +0300

    Added datatypes

---
+++ src/DatatypeJava.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+public class Datatypejava {
+    int a = 1;
+    byte byteNumber = 120;
+    long longNumber = 54523523;
+    short shortNumber = -3000;
+    float decimalNumber = 2.45f;
+    double doubleDecimalNumber = 3.456d;
+
+    //Character
+    char alpha = 'F'
+
+            //True/False
+    boolean hadLunch = true; // had lunch?
+    boolean isHoliday = false;
+
+    int i; // o
+
+    float x; // o.oof
+
+    char animal; // ''
+
+    boolean isClassTomorrow; //false
+
+}
