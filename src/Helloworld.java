961898ae6d345402329035f0ab4e571b1a89864e
Enn18 <ennennuste3@gmail.com>
2022-09-14 19:24:10 +0300

    Hello program implemented

---
+++ src/Helloworld.java
@@ -0,0 +1,9 @@
+/**
+ * The first hello world program
+ * @author Ennuste Enn
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hellow, World!");
+    }
+}
