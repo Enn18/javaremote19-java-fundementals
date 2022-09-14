fe8a5b593f9ef6ac97035c082819c2ab8166a544
Enn18 <ennennuste3@gmail.com>
2022-09-14 20:53:49 +0300

    Added datatype examples

---
+++ src/Datatype.java
@@ -0,0 +1,80 @@
+import java.sql.SQLOutput;
+
+/**
+ *
+ */
+public class Datatype {
+
+    //Primitivedatatypes
+    //numbers
+    int a = 1;
+    byte byteNumber = 120;
+    long longNumber = 54523523;
+    short shortNumber = -3000;
+    float decimalNumber = 2.45f;
+    double doubleDecimalNumber = 3.456d;
+
+    //Character
+    char alpha = 'F';
+
+            //True/False
+    boolean hadLunch = true; // had lunch?
+    boolean isHoliday = false;
+
+    int i; // o
+
+    float x; // o.oof
+
+    char animal; // ''
+
+    boolean isClassTomorrow; //false
+
+    //Non_primitive data types
+    String word = "Im a developer";
+
+    Character random = 'U';
+
+    Float numberSecond = 2.45F;
+
+    Integer randomNumber; //null
+
+    //Multi_declaration
+    int number0ne, numberTwo, numberThree;
+
+            String fruit = "apple", vegetable = "pumpkin", cities;
+
+
+           //Scope of class variables
+            private static String CAR = "Toyota";
+
+            public static String FOOD = "Cake";
+
+
+    public static void main(String[] args) {
+        int k = 0;
+        Integer y = null;
+
+
+        System.out.println(k);
+        System.out.println(y);
+
+
+        String _color = "Black";
+
+        int $hexanumber = 0x2A;
+
+        float exam12_marks = 100.10f;
+
+        String className ="sda";
+
+        int a = 100;
+        int b = 300;
+
+        int c = a + b;
+        System.out.println(CAR);
+        System.out.println(FOOD);
+
+    }
+
+
+}
--- src/Helloworld.java
+++ src/Helloworld.java
@@ -5,5 +5,7 @@
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hellow, World!");
+
+        System.out.println(Datatype.FOOD);
     }
 }
