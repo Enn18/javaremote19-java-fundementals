3721bb7141f114c9e74ed4179626da51510d0440
Enn18 <ennennuste3@gmail.com>
2022-09-17 00:20:01 +0300

    BankingApplication

---
+++ src/OperatorHomework.java
@@ -0,0 +1,30 @@
+public class OperatorHomework {
+    public static void main(String[] args) {
+        float i = 9;
+        float a = 18;
+
+        float result = i * a + (--i + ++a) + i % a;
+        System.out.println(result);
+
+        /*
+        1.
+        2.
+        3.
+        4.
+        5.
+        6.
+        7.
+         */
+
+
+
+
+
+        //when finished push to github
+
+    }
+
+
+
+
+}
