34a641a98ecf62b7a06fdaf47c4d9afea3b650c9
Enn18 <ennennuste3@gmail.com>
2022-09-25 16:57:53 +0300

    homework4

--- Homework4.java
+++ Homework4.java
@@ -11,9 +11,9 @@ public class Homework4 {
          int sum = 0;
 
          for (int num: Array){
-             sum = sum + num;
+             sum = sum + num; // this means sum sum = 0+1, then sum= 1+7, sum = 8+3, sum= 11 +10, sum =21+9
          }
-         int average = sum / Array.length;
+         int average = sum / Array.length;    // here the 30 is devided with how many numbers are in array.
         System.out.println("The average is" + average);
 
 
