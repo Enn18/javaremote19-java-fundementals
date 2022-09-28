4f6a86156c4afd17d18930ffccfed7b290f3198f
Enn18 <ennennuste3@gmail.com>
2022-09-28 19:54:50 +0300

    Object examples added

---
+++ src/exercises/StatementExercise.java
@@ -0,0 +1,18 @@
+package exercises;
+
+public class StatementExercise {
+    public static void main(String[] args) {
+
+        // if
+        //Write A java program to find if the person is adult. (minimum adult age is 18)
+
+
+        int myAge = 21;
+
+        if( myAge >= 18) {
+            System.out.println("The person is an adult");
+        }else{
+            System.out.println("The person is not an adult");
+        }
+    }
+}
