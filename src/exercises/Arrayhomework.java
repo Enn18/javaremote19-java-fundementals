0d630ca654540b5ead5b80ac46be71486d9f2304
Enn18 <ennennuste3@gmail.com>
2022-09-28 18:10:48 +0300

    Object examples added

---
+++ src/exercises/Arrayhomework.java
@@ -0,0 +1,15 @@
+package exercises;
+
+/**
+ * Write a Java program to find the second largest element in an array
+ *
+ * @author Enn Ennuste
+ */
+
+public class Arrayhomework {
+    public static void main(String[] args) {
+        int[] a = {1, 7, 3, 10, 9};
+
+
+    }
+}
